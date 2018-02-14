c5d971281b3cceb6c9249763d66a8bb023a86c97
yousseftarekkh <yousseftarek@live.com>
2018-02-14 18:37:59 +0200

    init() updated

--- src/Potatoz/DBApp.java
+++ src/Potatoz/DBApp.java
@@ -2,20 +2,26 @@ package Potatoz;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.*;
 
 public class DBApp {
 	PrintWriter writer;
 	StringBuilder sb;
+	File location;
 	Hashtable<String, Hashtable<String, String>> restrictions;
 
-	public void init() throws FileNotFoundException {
-
+	public void init() throws IOException {
+		new File("data").mkdirs();
+		new File("data/metadata.csv").createNewFile();
+		new File("docs").mkdirs();
+		new File("classes").mkdirs();
+		new File("config").mkdirs();
 	}
 
 	public void createTable(String strTableName, String strClusteringKeyColumn,
-		Hashtable<String, String> htblColNameType) throws FileNotFoundException {
+			Hashtable<String, String> htblColNameType) throws FileNotFoundException {
 		htblColNameType.keySet().remove(strClusteringKeyColumn);
 		writer = new PrintWriter(new File(strTableName + ".csv"));
 		sb = new StringBuilder();
@@ -30,7 +36,7 @@ public class DBApp {
 	}
 
 	public void insertIntoTable(String strTableName, Hashtable<String, Object> htblColNameValue) {
-		
+
 	}
 
 	public void updateTable(String strTableName, String strKey, Hashtable<String, Object> htblColNameValue) {
@@ -42,6 +48,11 @@ public class DBApp {
 	public Iterator selectFromTable(String strTableName, String strColumnName, Object[] objarrValues,
 			String[] strarrOperators) {
 		return null;
+	}
+
+	public static void main(String[] args) throws IOException {
+		DBApp app = new DBApp();
+		app.init();
 	}
 
 }
